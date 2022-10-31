3fd4ac6b67920ee14bd3206a6304725089c18aaa
Mike <mwroberts89@gmail.com>
2022-10-31 09:25:13 -0400

    YNAB OAUTH basics are in place, need to edit fetch request to use temp token

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Balance from "./components/Balance"
 import ControlPanel from "./components/control-panel/ControlPanel"
 import FirstTimeSetup from "./components/FirstTimeSetup"
@@ -7,44 +8,57 @@ import { useGlobalContext } from './context'
 
 function App() {
   const { isFirstTime } = useGlobalContext();
+  const [token, setToken] = useState('');
 
-  if (isFirstTime) {
-
-    console.log(window.location.href);
-
+  if (!token) {
     if (window.location.href.includes('token=')) {
       let access_token = window.location.href.split('http://localhost:3000/#access_token=');
+      // let access_token = window.location.href.split('https://ynab-daily-spender.netlify.app/#access_token=');
       access_token = access_token[1].split('&token_type=Bearer&expires_in=7200');
       access_token = access_token[0];
-      console.log(access_token);
+      setToken(access_token);
+      // window.location.href = 'http://localhost:3000'
     }
-
-    // 'https://app.youneedabudget.com/oauth/authorize?client_id=_4jwptmM2jY9dfy0j2f7AnUSMHJjIDcGEIgqw9nYiiM&redirect_uri=http://localhost:3000/&response_type=token'
-
+    
     // will need to check if authorized by YNAB user and request a token if needed, regardless if first or nth time using app
     return (
-      <a href='https://app.youneedabudget.com/oauth/authorize?client_id=_4jwptmM2jY9dfy0j2f7AnUSMHJjIDcGEIgqw9nYiiM&redirect_uri=http://localhost:3000/&response_type=token'>please authorize use of Daily Spender</a>
+      <div className='App'>
+        <div className='app-wrapper'>
+          <Header />
+          <section className='app-login'>
+            <button>
+              <a href='https://app.youneedabudget.com/oauth/authorize?client_id=_4jwptmM2jY9dfy0j2f7AnUSMHJjIDcGEIgqw9nYiiM&redirect_uri=http://localhost:3000/&response_type=token'>please authorize use of Daily Spender</a>
+            </button>
+            {/* <a href='https://app.youneedabudget.com/oauth/authorize?client_id=_4jwptmM2jY9dfy0j2f7AnUSMHJjIDcGEIgqw9nYiiM&redirect_uri=https://ynab-daily-spender.netlify.app/&response_type=token'>please authorize use of Daily Spender</a> */}
+          </section>
+        </div>
+        <Footer />
+      </div>
     )
-    
 
+  }
 
-    // return (
-    // <div className="App">
-    //   <Header />
-    //   <FirstTimeSetup />
+  if (isFirstTime) {
+    return (
+    <div className="App">
+      <div className='app-wrapper'>
+        <Header />
+        <FirstTimeSetup />
 
-    //   <section style={{display: 'none'}}>
-    //     <Balance /> 
-    //   </section>
-    // </div>
-    // );
+        <section style={{display: 'none'}}>
+          <Balance token={token}/> 
+        </section>
+      </div>
+      <Footer />
+    </div>
+    );
   }
 
   return (
     <div className="App">
       <div className="app-wrapper">
         <Header />
-        <ControlPanel />
+        <ControlPanel token={token}/>
       </div>
         <Footer />
     </div>
